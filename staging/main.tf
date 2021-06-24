4bba3915bf566f0e78206cc6a2fa77c79c36c653
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-06-24 09:20:43 +0100

    Add main tf file

---
+++ staging/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 2.0"
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+locals {
+    service_name = "mtfh-finance"
+    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+}
+
+
+# Terraform State Management
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-housing-development"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/mtfh-finance-infrastructure/state"
+  }
+}
