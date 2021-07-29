bfe4920e3ec2aec81c5004a0587b9fd6598cd251
LBHSPreston <31885748+LBHSPreston@users.noreply.github.com>
2021-07-29 10:11:51 +0100

    add staging and production postgres configuration

---
+++ production/postgresdb.tf
@@ -0,0 +1,22 @@
+# Postg Database Setup
+
+module "postgres_db_production" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/postgres"
+  environment_name = "production"
+  vpc_id =  "vpc-0ce853ddb64e8fb3c"
+  db_engine = "postgres"
+  db_engine_version = "12.5"
+  db_identifier = "mtfh-finance-pgdb"
+  db_instance_class = "db.t3.large"
+  db_name = data.aws_ssm_parameter.housing_finance_postgres_database.value
+  db_port  = data.aws_ssm_parameter.housing_finance_postgres_port.value
+  db_username = data.aws_ssm_parameter.housing_finance_postgres_username.value
+  db_password = data.aws_ssm_parameter.housing_finance_postgres_password.value
+  subnet_ids = ["subnet-0beb266003a56ca82","subnet-06a697d86a9b6ed01"]
+  db_allocated_storage = 100
+  maintenance_window ="sun:10:00-sun:10:30"
+  storage_encrypted = true
+  multi_az = true //only true if production deployment
+  publicly_accessible = false
+  project_name = "housing finance"
+}
