80b46f8a32e131fd872a310c2e0a23ed2030c562
LBHSPreston <31885748+LBHSPreston@users.noreply.github.com>
2021-07-29 09:42:12 +0100

    change reference to vpc and security group

--- development/postgresdb.tf
+++ development/postgresdb.tf
@@ -8,9 +8,9 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 module "postgres_db_development" {
-  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git/modules/database/postgres"
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/postgres"
   environment_name = "development"
-  vpc_id = data.aws_vpc.development_vpc.id
+  vpc_id =  "vpc-0d15f152935c8716f"
   db_engine = "postgres"
   db_engine_version = "12.5"
   db_identifier = "mtfh-finance-pgdb-development"
@@ -19,7 +19,7 @@ module "postgres_db_development" {
   db_port  = data.aws_ssm_parameter.housing_finance_postgres_port.value
   db_username = data.aws_ssm_parameter.housing_finance_postgres_username.value
   db_password = data.aws_ssm_parameter.housing_finance_postgres_password.value
-  subnet_ids = data.aws_subnet_ids.development_private_subnets.ids
+  subnet_ids = ["subnet-05ce390ba88c42bfd","subnet-0140d06fb84fdb547"]
   db_allocated_storage = 20
   maintenance_window ="sun:10:00-sun:10:30"
   storage_encrypted = false
