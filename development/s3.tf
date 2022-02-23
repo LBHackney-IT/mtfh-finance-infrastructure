bcfce99451bcd67271c84bcadd085043f2d152ee
= <Selwyn.Preston@hackney.gov.uk>
2022-02-23 10:22:14 +0000

    add S3 configuration

---
+++ development/s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "mtfh_print" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "mtfh-print-${var.environment_name}"
+    Environment = "${var.environment_name}"
+  }
+}
+
+resource "aws_s3_bucket_acl" "print_acl" {
+  bucket = aws_s3_bucket.mtfh_print.id
+  acl    = "private"
+}
+
+# TODO: - Permissions to be clarified on who has access, and how would they access to create bucket policy
+#       - Object lifecycle; how long do we keep objects in the bucket for and what happens when they expire
+#       - Versioning; would these objects change?  If so do we need to keep the older versions?
+#       - Enryption; what level of encryption do we need?
