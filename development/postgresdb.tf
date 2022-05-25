18391b6f76cfffba30b58e604f7d4950e9d03467
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2022-05-25 15:16:37 +0100

    deprecated postgres version
    In response to error "Cannot find upgrade path from 12.8 to 12.5."

--- development/postgresdb.tf
+++ development/postgresdb.tf
@@ -5,7 +5,7 @@ module "postgres_db_development" {
   environment_name = "development"
   vpc_id =  "vpc-0d15f152935c8716f"
   db_engine = "postgres"
-  db_engine_version = "12.5"
+  db_engine_version = "12.8"
   db_identifier = "mtfh-finance-pgdb"
   db_instance_class = "db.t3.micro"
   db_name = data.aws_ssm_parameter.housing_finance_postgres_database.value
