042ca9d642095c4ace0a5b2b9428a15e2ca5c52e
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2022-05-25 12:15:26 +0100

    redo lambda trigger event

--- civica_sftp_filesync_module/s3.tf
+++ civica_sftp_filesync_module/s3.tf
@@ -32,13 +32,21 @@ data "aws_lambda_function" "existingCashfileLambda" {
   function_name = var.statemachine_lambda_name
 }
 
-# # 2) Add a bucket lambda trigger for the PUT event
-# resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
-#   bucket = aws_s3_bucket.sftpbucket.id
-#   lambda_function {
-#     lambda_function_arn = "arn:aws:lambda:eu-west-2:364864573329:function:housing-finance-interim-api-development-check-cash-files" #data.aws_lambda_function.existingCashfileLambda.arn
-#     events              = ["s3:ObjectCreated:*"]
-#     filter_prefix       = "${var.bucket_folder_name}/"
-#     filter_suffix       = ".dat"
-#   }
-# }
+# 2) Add a bucket lambda trigger for the PUT event
+resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+  bucket = aws_s3_bucket.sftpbucket.id
+  lambda_function {
+    lambda_function_arn = data.aws_lambda_function.existingCashfileLambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "${var.bucket_folder_name}/"
+    filter_suffix       = ".dat"
+  }
+}
+
+resource "aws_lambda_permission" "test" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.existingCashfileLambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${aws_s3_bucket.sftpbucket.id}"
+}
