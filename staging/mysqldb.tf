41d01ac0be3216e7266b792723163fb3e38fb309
Selwyn Preston <Selwyn.Preston@hackney.gov.uk>
2022-05-31 15:09:59 +0100

    Add db read replicas to staging and production environments

--- staging/mysqldb.tf
+++ staging/mysqldb.tf
@@ -40,3 +40,32 @@ resource "aws_db_instance" "housing-mysql-db" {
     project_name      = "MTFH Finance"
   }
 }
+
+resource "aws_db_instance" "housing-mysql-db-replica" {
+  identifier                  = "housing-finance-db-${var.environment_name}-replica"
+  replicate_source_db         = aws_db_instance.housing-mysql-db.id
+  instance_class              = "db.t2.micro" //this should be a more production appropriate instance in production
+  allocated_storage           = 10
+  storage_type                = "gp2" //ssd
+  port                        = 3306
+  backup_window               = "00:01-00:31"
+  vpc_security_group_ids      = [aws_security_group.mtfh_finance_security_group.id]
+  monitoring_interval         = 0 //this is for enhanced Monitoring there will already be some basic monitoring available
+  backup_retention_period     = 30
+  storage_encrypted           = false  //this should be true for production
+  deletion_protection         = false
+  multi_az                    = false //this should be true for production
+  auto_minor_version_upgrade  = true
+  allow_major_version_upgrade = false
+
+  apply_immediately   = false
+  skip_final_snapshot = true
+  publicly_accessible = false
+
+  tags = {
+    Name              = "housing-finance-db-${var.environment_name}-replica"
+    Environment       = "${var.environment_name}"
+    terraform-managed = true
+    project_name      = "MTFH Finance"
+  }
+}
