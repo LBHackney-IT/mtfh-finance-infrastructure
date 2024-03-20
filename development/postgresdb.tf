3faf56938393ebdfef2de79a772b29bcda15d970
Adam <74552077+adamtry@users.noreply.github.com>
2024-03-20 15:25:57 +0000

    Fix some names

--- development/postgresdb.tf
+++ development/postgresdb.tf
@@ -17,16 +17,14 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 resource "aws_db_instance" "postgres_db_development" {
-  environment_name = "development"
-  vpc_id =  "vpc-0d15f152935c8716f"
-  db_engine = "postgres"
-  db_engine_version = "12.14"
-  db_identifier = "mtfh-finance-pgdb"
-  db_instance_class = "db.t3.micro"
+  engine = "postgres"
+  engine_version = "12.14"
+  identifier = "mtfh-finance-pgdb-db-development"
+  instance_class = "db.t3.micro"
   db_name = data.aws_ssm_parameter.housing_finance_postgres_database.value
-  db_port  = data.aws_ssm_parameter.housing_finance_postgres_port.value
-  db_username = data.aws_ssm_parameter.housing_finance_postgres_username.value
-  db_password = data.aws_ssm_parameter.housing_finance_postgres_password.value
+  port  = data.aws_ssm_parameter.housing_finance_postgres_port.value
+  username = data.aws_ssm_parameter.housing_finance_postgres_username.value
+  password = data.aws_ssm_parameter.housing_finance_postgres_password.value
   db_subnet_group_name = aws_db_subnet_group.db_subnets.name
   allocated_storage = 20
   maintenance_window ="sun:10:00-sun:10:30"
