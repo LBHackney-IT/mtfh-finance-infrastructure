2837c5c5b0806ee4315aaa82b085d05d3cdb600a
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-03-25 10:29:40 +0000

    remove source db

--- production/mssqldb.tf
+++ production/mssqldb.tf
@@ -8,13 +8,13 @@ resource "aws_db_subnet_group" "mssql_db_subnets" {
 }
 
 # identifier for the Source database
-data "aws_db_instance" "source_db" {
-  db_instance_identifier = "housing-finance-sql-db-production"
-}
+# data "aws_db_instance" "source_db" {
+#   db_instance_identifier = "housing-finance-sql-db-production"
+# }
 
 # Snapshot1 - create a snapshot of the Source DB
 data "aws_db_snapshot" "db1_snapshot" {
-  db_instance_identifier = data.aws_db_instance.source_db.id
+  # db_instance_identifier = data.aws_db_instance.source_db.id
   db_snapshot_identifier = "arn:aws:rds:eu-west-2:282997303675:snapshot:housing-finance-sql-prod-web-snapshot"
 }
 
