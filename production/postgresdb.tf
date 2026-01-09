ac907e4bc97e293b157eae0373344efa9e632cfd
Adam <adam.tracy20@gmail.com>
2026-01-09 14:06:23 +0000

    Update MySQL and PostgreSQL configurations; comment out Redis resources

--- production/postgresdb.tf
+++ production/postgresdb.tf
@@ -4,18 +4,18 @@
 module "postgres_db_production" {
   source = "../modules/postgres"
   environment_name = "production"
-  vpc_id =  "vpc-0ce853ddb64e8fb3c"
+  vpc_id =  "vpc-006989d0b2bb070d9"
   db_engine = "postgres"
   db_engine_version = "16.8"
   db_parameter_group_name = "postgres16"
-  db_identifier = "mtfh-finance-pgdb"
-  db_instance_class = "db.t3.large"
+  db_identifier = "uh-mirror-db"
+  db_instance_class = "db.t3.medium"
   db_name = data.aws_ssm_parameter.housing_finance_postgres_database.value
   db_port  = data.aws_ssm_parameter.housing_finance_postgres_port.value
   db_username = data.aws_ssm_parameter.housing_finance_postgres_username.value
   db_password = data.aws_ssm_parameter.housing_finance_postgres_password.value
   subnet_ids = ["subnet-05e595c59b7d6c8df","subnet-0e6bc9b4ac24493cc"]
-  db_allocated_storage = 100
+  db_allocated_storage = 20
   maintenance_window ="sun:10:00-sun:10:30"
   backup_window = "00:01-00:31"
   storage_encrypted = true
@@ -23,5 +23,5 @@ module "postgres_db_production" {
   publicly_accessible = false
   project_name = "housing finance"
   backup_policy = "Prod"
-  vpc_security_group_ids = ["sg-07d40f16ad18f1f60"]
+  vpc_security_group_ids = ["sg-0de9adcce8b43b894"]
 }
